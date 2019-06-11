c7a4642edf694c959c1568d34809369f21a7fd4b
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-11 12:33:33 -0400

    Completed lab

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class AppointmentsController < ApplicationController
+  def index
+    @appointments = Appointment.all
+  end
+
+  def show
+    @appointment = Appointment.find(params[:id])
+    @doctor = @appointment.doctor
+    @patient = @appointment.patient
+  end
+end
